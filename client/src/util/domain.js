2e5d5f6b46ca3ac05da714f2f5caec23cece08f8
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-11-14 18:02:12 +0200

    c

--- client/src/util/domain.js
+++ client/src/util/domain.js
@@ -1,4 +1,3 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
-  : process.env.NODE_ENV === "production" &&
-    "https://snippet-manager-server.herokuapp.com";
+  : process.env.NODE_ENV === "production" && "https://pfplanner.herokuapp.com/";
