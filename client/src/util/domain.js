70d8e143894e775ef8845b165e2f8a4bd6ed08f2
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-11-14 18:16:20 +0200

    Update domain.js

--- client/src/util/domain.js
+++ client/src/util/domain.js
@@ -1,3 +1,5 @@
-export default process.env.NODE_ENV === "development"
+/* export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
   : process.env.NODE_ENV === "production" && "https://pfplanner.herokuapp.com/";
+ */
+export default "https://pfplanner.herokuapp.com";
