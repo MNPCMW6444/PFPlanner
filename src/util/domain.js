c98e67c9fd0250642cc5bf4596c43fc0f7af5684
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2021-11-19 23:27:12 +0200

    Update domain.js

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,5 +1,5 @@
 let enva = process.env.NODE_ENV;
-enva = "production"; // comment this by need
+enva = "development"; // comment this by need
 export default enva === "development"
   ? "http://localhost:5000"
   : enva === "production" && "https://pfplanner.herokuapp.com";
